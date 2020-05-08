1e462055cb5ce6645202d15072deacc7490f4b77
Atri Das <babinsamrat123@gmail.com>
2020-05-08 12:59:29 +0530

    tests well

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: ListView.builder(
           shrinkWrap: true,
